b11f571f95d3e14e7734b8a4bcc0690ce6a3cfae
nkai842 <68232424+nkai842@users.noreply.github.com>
2020-07-18 11:01:18 -0400

    Update stockpull.py

--- stockpull.py
+++ stockpull.py
@@ -2,12 +2,21 @@
 from YahooFin import DataYahoo
 import pandas as pd
 
+
 ticker = ['CROX','AAPL','AIG','BYND',
 'ROKU','CPB']
 
+# sets "t" as YahooFin module
+
 t = DataYahoo(ticker)
+
+## Calls pull_data() method from YahooFin
+
 po = t.pull_data()
 
+## Creates a data frame and prints
 df = pd.DataFrame(po)
 print(df)
+
+## Imports data frame to csv file to then manipulate in excel
 df.to_csv('stocks.csv', index = True)
